7a065188afb8327e5d62b23fd895cc11a4f39dea
Nikolai Serdyuk <flaer645@gmail.com>
2022-09-19 15:34:29 +0300

    added Lesson 2

---
+++ Lesson02/Example002_IntroArray2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 122, 23, 4, 54, 63, 122, 72, 81};
+
+int n = array.Length;
+int find = 122;
+
+int index = 0;
+while(index < n)
+{
+    
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
