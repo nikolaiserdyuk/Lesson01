1ad833a8b6a35a6b7af4f07fe800bc54319c763a
Nikolai Serdyuk <flaer645@gmail.com>
2022-09-22 15:01:05 +0300

    added first exercise of the second homework.

---
+++ Homework02/Exercise01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstdigit = num / 100;
+int seconddigit = (num /10) % 10;
+int thirddigit = num % 10;
+
+
+Console.WriteLine(seconddigit);
