3eb82843340af80cb40c67778aa0a2f1b36440b7
Nikolai Serdyuk <flaer645@gmail.com>
2022-09-27 18:25:11 +0300

    added lesson03

---
+++ lesson03/Example001/Program.cs
@@ -0,0 +1,103 @@
+﻿//Methods
+
+//Вид 1 Метод ничего не принимает и ничего не возвращает
+void Method1()
+{
+    Console.Writeline("Author ...");
+}
+Method1();    //так метод можно вызвать. нужно не забыть про круглые скобки, иначе выдаст ошибку
+
+
+
+
+
+//Вид 2.1 Метод принимает какие-то аргументы, но ничего не возвращает
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения"); //так можно вызвать метод
+
+//Вид 2.2 Метод принимает аргументы текста и числовые
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;     // увеличение счетчика на 1 называется "инкриментом". а уменьшение счетчика на 1 называют "декриментом"
+    }
+}
+Method2("Текст", 4); //так можно вызвать метод, текст "текст" будет прописан четыре раза.
+Method2(count: 4, msg : "новый текст"); //а можно и так, используя "именнованные" аргументы
+
+
+// Вид 3. Метод ничего не принимает, но возвращает
+//если метод что-то возвращает, обязательно указать тип данных!
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();   //так мы переменной присваеваем работу метода которую он произвел выше.
+Console.WriteLine(year);
+
+
+// Вид 4. Метод что-то принимает и что-то возвращает. Самый важный и нужный метод. 
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text; 
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
+
+// ЦИКЛЫ WHILE AND FOR
+// FOR ---------------    позволяет условно "упростить" 4 вид метода
+// for(int i = 0, i < 10, i++)
+// {
+//      Console.WriteLine(i);   
+// }
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for( int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "z");
+Console.WriteLine(res);
+
+
+// ЦИКЛЫ В ЦИКЛАХ
+// for (int i = 0; i < 10; i++)
+//{
+//    for (int j = 0; j < 10, i++)
+//    {
+//         Console.WriteLine(i*j);
+//    }
+//    Console.WriteLine();
+//}
+
+// ПОСТРОИМ ТАБЛИЦУ УМНОЖЕНИЯ ЧЕРЕЗ ЦИКЛ В ЦИКЛЕ:
+
+for (int i = 2; i <= 10; i++)         //i начинаем с 2 как в таблице умножения
+{
+    for(int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();              // просто переходна новую строчку для таблицы умножения. по идее, таблицу умножения мы уже написали.
+}
---
+++ lesson03/Example002/Program.cs
@@ -0,0 +1,42 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+// Что значит "дан текст"? 
+// Что значит "черточками"?
+// Какого алфавита?
+// Маленькие буквы "к" заменить большими "К" и большие "С" заменить маленькими "с".
+
+string text = "- Я думаю, сказал князь, улыбаясь, - что "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             0123456789
+// s[3] // r     // третьим символом будет буква "r"
+
+string Replace(string text, char oldValue, char newValue);
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine();
+newText = Replace(text, 'С', 'с');
+Console.WriteLine();
+
+//код полностью повторен за преподавателем, и, конечно же, не работает =)
