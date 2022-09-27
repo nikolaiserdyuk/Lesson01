9ebd0886e232078bce05d18d734cdfcaf4af03fb
Nikolai Serdyuk <flaer645@gmail.com>
2022-09-27 19:28:53 +0300

    added seminar02

---
+++ seminar02/Ex002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу которая выводит случайноe число из отрезка [10, 99] и показывает наибольшую цифру числа
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int n = new Random().Next(10, 100);
+Console.WriteLine("Здравствуй, пользователь!");
+Console.WriteLine("Загадано число ");
+Console.WriteLine(n);
+
+int a = n / 10;
+int b = n % 10;
+
+Console.WriteLine("Максимальная цифра: ");
+if(a  > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
---
+++ seminar02/Ex003/Program.cs
@@ -0,0 +1,29 @@
+﻿// Определить присутствует ли в заданном массиве некоторое число
+// 1. массив задается автоматически
+// 2. искомое число загадывается компьютером
+
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0;
+Console.Write("Ищем = ");
+Console.WriteLine(find);
+Console.WriteLine("Массив: ");
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 101);
+    Console.WriteLine(array[index] + " ");
+    index++;
+}
+
+index = 0;
+while (index < count)
+{
+    if(array[index] = find)
+    {
+        Console.WriteLine("Такой элемент есть");
+    }
+    index++;
+}
+Console.WriteLine("\nEnd");
